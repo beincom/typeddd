6e19a6db0e72896d6ce8e42649e9131bc1bdb617
The <thevan@tgm.vn>
2022-10-05 02:53:51 +0700

    fix(core): npm script

---
+++ packages/domain/src/index.ts
@@ -0,0 +1,7 @@
+export * from './utils';
+export * from './value-objects';
+export * from './entities';
+export * from './exceptions';
+export * from './factories';
+export * from './interfaces/domain';
+export * from './interfaces/ports';
