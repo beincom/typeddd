f960d1891bdf71178c4794fdc34cd4a1f8a4af4a
The <thevan@tgm.vn>
2022-10-17 19:09:28 +0700

    build(infra): add request id

--- packages/sequelize-uow/src/request-context.register.ts
+++ packages/sequelize-uow/src/request-context.register.ts
@@ -1,5 +1,7 @@
+import { generateUUID } from '@typeddd/common';
 import { IRequest, IResponse, RequestContext } from './request-context';
 
 export function registerRequestContext(req: IRequest, res: IResponse, next: () => void) {
+  req.id = generateUUID();
   RequestContext.cls.run(new RequestContext(req, res), next);
 }
